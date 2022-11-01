ceaebe990f0804ff2acefc87ea05d070ea0bd0aa
UTSAV PATEL <patelutsav3732@gmail.com>
2022-11-01 22:30:49 +0530

    File has been updated with some new codes

--- intro.js
+++ intro.js
@@ -153,6 +153,104 @@ function testGreaterthen(val){
 }
 console.log(testGreaterthen(10000));
 
+//Comparison with the less than oprator 
+function testlessthan(val){
+    if(val<25){
+        return "less than 25 ";
+    }
+    if(val<55){
+    return "less than 55"
+    }
+    return "55 or over"
+}
+console.log(testlessthan(10));
+
+// Comparison with the less than or equal to operator 
+function testlessthanorequal(val){
+    if(val<=12){
+        return "smaller than or equal to 12"
+    }
+    if(val<=24){
+        return "smaller than or equal to 24"
+    }
+    return "more than 24"
+}
+console.log(testlessthanorequal(10));
+
+// Comparison with the logical and Operator
+function testLogicalAnd(val){
+    if(val<=15 && val>=25){
+        return "Yes"
+    }
+    return "No"
+}
+console.log(testLogicalAnd(20));
+
+// Comparison with the Logical or Operator
+function testLogicalOr(val){
+    if(val<10 || val>20){
+        return "Outside";
+    }
+    return "Inside";
+}
+console.log(testLogicalOr(50));
+
+// Else Statment 
+function testElse(val){
+    var result = ""
+    if(val<5){
+        result ="Bigger than 10"
+    } else{
+        result ="5 or smaller "
+    }
+    return result ;
+}
+console.log(testElse(5));
+
+// Else if Statement
+function testElseIf(val){
+    if(val>10){
+        return "Greater than 10"
+    } else if(val<5){
+        return "Smaller than 5"
+    } else{
+        return "Between 5 or 10"
+    }
+}
+console.log(testElseIf(6));
+
+// Logical Order in IF Else Statement
+function orderMyLogical(val){
+    if(val<5){
+        return "Less than 5";
+    } else if (val<10){
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10"
+    }
+}
+console.log(orderMyLogical(5));
+
+// Chaining IF ELSE statement
+function testSize(num){
+    if(num<5){
+        return "UTSAV";
+    }else if(num<10){
+        return "Small"
+    } else if(num<15){
+        return "Medium"
+    } else if(num<20){
+        return "Large"
+    }else {
+        return "Huge"
+    }
+}
+console.log(testSize(20));
+
+// GOLF CODE 
+
+
+
 
 
 
